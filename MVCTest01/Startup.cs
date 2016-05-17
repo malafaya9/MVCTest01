108f7e18fcda2532d6ff572f96da91c632f0ab64
malafaya9 <malafaya9@gmail.com>
2016-05-17 14:50:50 -0300

    Add project files.

---
+++ MVCTest01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTest01.Startup))]
+namespace MVCTest01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
